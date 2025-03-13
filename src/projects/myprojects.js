ba32d8504cf452b16d2a0f3c24bd8a7e7f36625f
reobest <sabdelrahman071@gmail.com>
2025-03-13 17:59:13 +0200

    uPDATED

--- src/projects/myprojects.js
+++ src/projects/myprojects.js
@@ -1,6 +1,5 @@
 import img1 from '../images/project1.png'
 import img2 from '../images/project2.png'
-import img3 from '../images/project3.png'
 import img4 from '../images/project4.png'
 import img5 from '../images/project5.png'
 import img6 from '../images/project6.png'
@@ -24,14 +23,14 @@ const webprojects = [
             id:"numberthree",
             name:"React Js Slack-clone Chat App",
             description:"As a frontend React developer, I spearheaded the\n creation of a cutting-edge Slack Clone, redefining the landscape of team\n collaboration in the digital era. ",
-            img:img3,
+            img:img4,
             link:"https://fir-rayan-b57cf.web.app",
         },
         {
             id:"numberfour",
             name:"Next js e-commerce",
             description:"Digital Hippo offers a curated selection of digital tools and tech products designed to simplify your online experience. Discover\n software, gadgets, and accessories with easy navigation and efficient service—all at your fingertips.",
-            img:img4,
+            img:img7,
             link:"https://digital-hippo-lc7e.onrender.com",
         },
         {
@@ -47,13 +46,6 @@ const webprojects = [
             description:"are Pulse is a comprehensive management system designed for efficient healthcare administration. Featuring a user-friendly\n interface and streamlined functionality, it facilitates patient tracking, appointment scheduling, and medical record management, enhancing overall care coordination.",
             img:img6,
             link:"https://main--care-pulse-managment-system.netlify.app",
-        },
-        {
-            id:"numberseven",
-            name:"Next js CarHub",
-            description:"offers a sleek, user-friendly platform for buying and selling cars. With a responsive design and easy navigation, users can\n effortlessly browse listings, compare specs, and find detailed vehicle information, all from any device.",
-            img:img7,
-            link:"https://main--car-hub2024.netlify.app/",
         },
 ] 
 export default webprojects ;
